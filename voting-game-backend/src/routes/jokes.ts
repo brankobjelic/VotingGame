59ab1b84bf0b3998ad99e6ce2feb29838ba8c179
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2025-02-19 13:57:06 +0100

    add deleteJoke route

--- voting-game-backend/src/routes/jokes.ts
+++ voting-game-backend/src/routes/jokes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getRandomJoke, submitVote } from '../controllers/jokeController';
+import { getRandomJoke, submitVote, deleteJoke } from '../controllers/jokeController';
 
 const router = Router();
 
 router.get('/api/joke', getRandomJoke);
 router.post('/api/joke/:id', submitVote);
+router.delete('/api/joke/:id', deleteJoke);
 
 export default router;
