5a88c9401aa10be319ee6c64203e0ec8d323409d
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2025-02-19 14:16:02 +0100

    add updateJoke route

--- voting-game-backend/src/routes/jokes.ts
+++ voting-game-backend/src/routes/jokes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getRandomJoke, submitVote, deleteJoke } from '../controllers/jokeController';
+import { getRandomJoke, submitVote, deleteJoke, updateJoke } from '../controllers/jokeController';
 
 const router = Router();
 
 router.get('/api/joke', getRandomJoke);
 router.post('/api/joke/:id', submitVote);
 router.delete('/api/joke/:id', deleteJoke);
+router.put('/api/joke/:id', updateJoke);
 
 export default router;
